2a0c43619a7259a8b6e69b3b57bd9061a132bea5
Your Name <vathsalasivapalan@gmail.com>
2025-09-24 16:23:22 +0200

    get_data code is ready

--- get_data.py
+++ get_data.py
@@ -2,4 +2,38 @@ import os
 import sys
 import json
 
-from dotenv  import load_dotenv
+from dotenv  import load_dotenv 
+# pip install python-dotenv
+
+
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo  
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+
+class NetworkdataExtract():
+    def __init__(self):
+        try:
+            pass
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_tojson_convertor(self):
+        try:
+            pass 
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def pushing_data_to_mongodb(self):
+        try:
+            pass 
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    if __name__ == "__main__":
+        pass
