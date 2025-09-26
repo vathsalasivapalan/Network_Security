cce64587fda85e517b750b3fd2df40362c9899a6
Your Name <vathsalasivapalan@gmail.com>
2025-09-26 17:22:05 +0200

    The config and artifact are ready

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -19,11 +19,12 @@ print(MONGO_DB_URL)
 
 
 class DataIngestion:
-    def __init__(self,data_ingestion_config):
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            self.initiate_data_ingestion.config = data_ingestion_config 
+            self.data_ingestion_config=data_ingestion_config
         except Exception as e:
-            raise NetworkSecurityException(e, sys) 
+            raise NetworkSecurityException(e,sys)
+        
         
     def export_collection_as_dataframe(self):
         try:
@@ -89,13 +90,15 @@ class DataIngestion:
     # master function
     def initiate_data_ingestion(self):
         try:
-            dataframe = self.export_collection_as_dataframe()
-            dataframe = self.export_data_into_feature_store(dataframe)
+            dataframe=self.export_collection_as_dataframe()
+            dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe=dataframe)
             
             data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
             test_file_path=self.data_ingestion_config.testing_file_path)
             return data_ingestion_artifact
-           
+            
+            
         except Exception as e:
-            raise NetworkSecurityException(e, sys)
+            raise NetworkSecurityException(e,sys)
+        
