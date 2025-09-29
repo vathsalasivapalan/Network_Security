edeb770afb6d6a1119e6acbaaeff7a3ef2f89e9f
Your Name <vathsalasivapalan@gmail.com>
2025-09-29 15:02:07 +0200

    Stop tracking Artifacts directory

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -7,17 +7,16 @@ print(training_pipeline.ARTIFACT_DIR)
 
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
-        timestamp =timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name=training_pipeline.PIPELINE_NAME
         self.artifact_name=training_pipeline.ARTIFACT_DIR
-        self.artifact_dir =os.path.join(self.artifact_name,timestamp)
-        self.timestamp:str=timestamp
+        self.artifact_dir=os.path.join(self.artifact_name,timestamp)
+        self.timestamp: str=timestamp
         
-
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir: str = os.path.join(
-                training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
+                training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
             )
         self.feature_store_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
