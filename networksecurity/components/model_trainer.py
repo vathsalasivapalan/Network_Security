fc4429f5b824841333e1770a3a040ec9fd0b9b0d
Your Name <vathsalasivapalan@gmail.com>
2025-10-11 09:36:29 +0200

    model training preparation is done

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,3 +1,12 @@
-class ModelTrainer:
-    def __init__(self):
-        pass
+import os, sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging  
+
+from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, ModelTrainerArtifact
+
+from xgboost import XGBClassifier
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.main_utils.utils import save_object, load_object
+from networksecurity.utils.ml_utils.model import load_numpy_array_data
